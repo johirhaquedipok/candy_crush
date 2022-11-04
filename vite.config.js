ab7f130bf0aaf0f422a859f27dc64363c7d009c7
johir haque dipok <johirhaquedipok2@gmail.com>
2022-11-05 03:09:03 +0600

    initial files

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/modern_ui_ux_gpt3_jsm/",
+  plugins: [react()],
+});
