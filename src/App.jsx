bbb63cf09e6b150746637606050c055badf4632a
johir haque dipok <johirhaquedipok2@gmail.com>
2022-11-07 18:59:02 +0600

    column three chekcing and remove those matching items

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,53 @@ function App() {
     }
   };
 
+  // check matching for column of three
+  /*   const checkForColumnOfThree = () => {
+    for (let i = 0; i <= 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every((square) => currentColorArrangement[square]) ===
+        decidedColor
+      ) {
+        columnOfThree.forEach((square) => {
+          return (currentColorArrangement[square] = "");
+        });
+      }
+    }
+  }; */
+
   useEffect(() => {
     createBoard();
   }, []);
 
+  useEffect(() => {
+    const checkForColumnOfThree = () => {
+      for (let i = 0; i <= 47; i++) {
+        const columnOfThree = [i, i + width, i + width * 2];
+        const decidedColor = currentColorArrangement[i];
+
+        if (
+          columnOfThree.every((square) => currentColorArrangement[square]) ===
+          decidedColor
+        ) {
+          columnOfThree.forEach((square) => {
+            return (currentColorArrangement[square] = "");
+          });
+        }
+      }
+    };
+
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, [100]);
+    return () => clearInterval(timer);
+  }, [currentColorArrangement]);
+
   return (
-    <div className="max-w-screen-2xl bg-slate-600 ">
-      <h1 className="text-5xl text-white ">candy crush</h1>
+    <div className="max-w-screen-2xl bg-slate-600 py-5 ">
       <div
         className="flex flex-wrap"
         style={{ width: "560px", height: "560px", margin: "0 auto" }}
@@ -39,6 +79,7 @@ function App() {
         {currentColorArrangement.map((item, idx) => (
           <div key={idx} className={`${item} w-16 h-16 `}>
             {idx}
+            <img src="" alt="" className="w-full h-full" />
           </div>
         ))}
       </div>
