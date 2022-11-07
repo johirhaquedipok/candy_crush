9c23e058c1e6a783f199a278765b68ed24b5eca7
johir haque dipok <johirhaquedipok2@gmail.com>
2022-11-08 02:55:13 +0600

    filling the removed gaps

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,21 @@ function App() {
     }
   };
 
-  // check matching for column of three
-  /*   const checkForColumnOfThree = () => {
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  // move the squres
+  const moveIntoSquareBelow = () => {
+    for (let i = 0; i < 64 - width; i++) {
+      if (currentColorArrangement[i] + width === "") {
+        currentColorArrangement[i + width] = currentColorArrangement[i];
+        currentColorArrangement[i] = "";
+      }
+    }
+  };
+  // check for column 3
+  const checkForColumnOfThree = () => {
     for (let i = 0; i <= 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
       const decidedColor = currentColorArrangement[i];
@@ -35,40 +48,89 @@ function App() {
         columnOfThree.every((square) => currentColorArrangement[square]) ===
         decidedColor
       ) {
-        columnOfThree.forEach((square) => {
-          return (currentColorArrangement[square] = "");
-        });
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
       }
     }
-  }; */
+  };
+  // check for column 4
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i <= 37; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
 
-  useEffect(() => {
-    createBoard();
-  }, []);
-
-  useEffect(() => {
-    const checkForColumnOfThree = () => {
-      for (let i = 0; i <= 47; i++) {
-        const columnOfThree = [i, i + width, i + width * 2];
-        const decidedColor = currentColorArrangement[i];
-
-        if (
-          columnOfThree.every((square) => currentColorArrangement[square]) ===
-          decidedColor
-        ) {
-          columnOfThree.forEach((square) => {
-            return (currentColorArrangement[square] = "");
-          });
-        }
+      if (
+        columnOfFour.every((square) => currentColorArrangement[square]) ===
+        decidedColor
+      ) {
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
       }
-    };
+    }
+  };
 
+  // check for row 3
+  const checkForRowOfThree = () => {
+    for (let i = 0; i <= 64; i++) {
+      const RowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        RowOfThree.every((square) => currentColorArrangement[square]) ===
+        decidedColor
+      ) {
+        RowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  // check for row 4
+  const checkForRowOfFour = () => {
+    for (let i = 0; i <= 64; i++) {
+      const RowOfFour = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55, 61, 62, 63,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        RowOfFour.every((square) => currentColorArrangement[square]) ===
+        decidedColor
+      ) {
+        RowOfFour.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForRowOfFour();
       checkForColumnOfThree();
+      checkForRowOfThree();
+      moveIntoSquareBelow();
       setCurrentColorArrangement([...currentColorArrangement]);
-    }, [100]);
+    }, [1000]);
     return () => clearInterval(timer);
-  }, [currentColorArrangement]);
+  }, [
+    currentColorArrangement,
+
+    checkForColumnOfFour,
+    checkForRowOfFour,
+    checkForColumnOfThree,
+    checkForRowOfThree,
+    moveIntoSquareBelow,
+  ]);
 
   return (
     <div className="max-w-screen-2xl bg-slate-600 py-5 ">
@@ -77,9 +139,9 @@ function App() {
         style={{ width: "560px", height: "560px", margin: "0 auto" }}
       >
         {currentColorArrangement.map((item, idx) => (
-          <div key={idx} className={`${item} w-16 h-16 `}>
+          <div>
             {idx}
-            <img src="" alt="" className="w-full h-full" />
+            <img src="" alt={""} key={idx} className={`${item} w-16 h-16`} />
           </div>
         ))}
       </div>
